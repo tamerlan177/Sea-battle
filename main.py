fc850be14c33aab6ca7016edca2e950e7271f6ae
tamerlan177 <tama06olimp@gmail.com>
2024-11-27 12:12:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
 import random
 
-name = string(input())
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_board(board):
+    print("  A B C D E F G")
+    for i, row in enumerate(board):
+        print(f"{i + 1} " + " ".join(row))
